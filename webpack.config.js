07fbdd03ec0939878794650966d3c36baa3353e9
ShujiOba <shuji365630@gmail.com>
2018-03-06 00:18:58 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.join(__dirname, 'doc'),
+        path: path.join(__dirname, 'docs'),
         filename: '[name].js'
     },
     module: {
