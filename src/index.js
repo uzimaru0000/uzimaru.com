565fd53344ac8d2a8563fc1beb10b47c17c6b13c
ShujiOba <shuji365630@gmail.com>
2018-12-02 00:37:58 +0900

    [build] ver2.0

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@
 require('./index.html');
 require('./style.scss');
 require('./assets/icon2.png');
-require('./assets/link.png');
-require('./assets/work.png');
 
 import {Elm} from './Elm/Main.elm';
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,62 @@
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    output: {
-        path: `${__dirname}/dist`,
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                exclude: [/node_modules/, /elm-stuff/ ],
-                use: 'file-loader?name=[name].[ext]'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            {
-                test: /\.png$/,
-                exclude: [ /node_modules/, /elm-stuff/ ],
-                use: 'file-loader?name=[name].[ext]'
-            },
-            {
-                test: /\.elm$/,
-                exclude: [ /node_modules/, /elm-stuff/ ],
-                use: [
-                    {
-                        loader: 'elm-webpack-loader',
-                        options: {
-                            debug: true
+module.exports = (env, argv) => {
+    return {
+        entry: {
+            app: './src/index.js'
+        }, output: {
+            path: `${__dirname}/dist`,
+            filename: '[name].js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    exclude: [/node_modules/, /elm-stuff/ ],
+                    use: 'file-loader?name=[name].[ext]'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
                         },
-                    }
-                ]
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                },
+                {
+                    test: /\.png$/,
+                    exclude: [ /node_modules/, /elm-stuff/ ],
+                    use: 'file-loader?name=[name].[ext]'
+                },
+                {
+                    test: /\.elm$/,
+                    exclude: [ /node_modules/, /elm-stuff/ ],
+                    use: [
+                        {
+                            loader: 'elm-webpack-loader',
+                            options: {
+                                debug: argv.mode === 'dedevelopment'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "css/style.css",
+                chunkFilename: "css/[id].css"
+            })
+        ],
+        devServer: {
+            inline: true,
+            stats: 'errors-only',
+            historyApiFallback: {
+                index: '/'
             }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/style.css",
-            chunkFilename: "css/[id].css"
-        })
-    ],
-    devServer: {
-        inline: true,
-        stats: 'errors-only',
-        historyApiFallback: {
-            index: '/'
         }
     }
 };
