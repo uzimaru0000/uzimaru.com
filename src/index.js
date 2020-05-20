2b874caf7e3f4735dc35773d64c2cff6b3d13b21
uzimaru0000 <shuji365630@gmail.com>
2020-05-21 06:55:24 +0900

    :wrench: webpack -> parcel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-"use strict";
+'use strict';
 
-require("./index.html");
-require("./style.scss");
-require("./assets/icon2.png");
+import './assets/icon2.png';
+import { Elm } from './Elm/Main.elm';
 
-import { Elm } from "./Elm/Main.elm";
-
-const dirData = localStorage.getItem("dir");
-const main = document.getElementById("main");
+const dirData = localStorage.getItem('dir');
+const main = document.getElementById('main');
 const app = Elm.Main.init({ node: main, flags: JSON.parse(dirData) });
 
-app.ports.store.subscribe(x => {
-  localStorage.setItem("dir", JSON.stringify(x));
+app.ports.store.subscribe((x) => {
+  localStorage.setItem('dir', JSON.stringify(x));
 });
